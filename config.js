8f6c4788e656d58bab631d658c6e66ba77af0a0e
sami-42 <sami.freelancer42@gmail.cm>
2022-07-17 20:30:33 +0600

    commit

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const APPNAME = "Door";
   //  export const BACKEND = "http://127.0.0.1:8000";
    export const BACKEND = "https://dotzdemo.xyz/door-laravel";
 
-export const BACKENDAPI = `${BACKEND}/api`;
+export const BACKENDAPI = `${BACKEND}/api`; 
