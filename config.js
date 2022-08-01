4ccce96c4017ecd4ddc24b6e6f1da3241f87e3a8
sami-42 <78957317+sami-42@users.noreply.github.com>
2022-08-01 12:11:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const APPNAME = "Door";
       //export const BACKEND = "http://127.0.0.1:8000";
-      //export const BACKEND = "https://dotzdemo.xyz/door-laravel";
-     export const BACKEND = "https://67d1-203-89-108-11.ngrok.io/";
+      export const BACKEND = "https://dotzdemo.xyz/door-laravel";
+
 
 export const BACKENDAPI = `${BACKEND}/api`;
 export const CURRENCY="£";
